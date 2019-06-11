63c3ffdbd851dc321387dcfd24ee103367aa07d5
Jianlong Hu <hujianlongynu@163.com>
2019-06-11 21:30:02 +0800

    初始化

---
+++ src/main/java/com/ynu/finalwork/FinalworkApplication.java
@@ -0,0 +1,13 @@
+package com.ynu.finalwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ynu/finalwork/FinalworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ynu.finalwork;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinalworkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
