7acd6775a09ad5385ed6f965eec1ed7b45c7056e
Jianlong Hu <hujianlongynu@163.com>
2019-06-22 10:48:48 +0800

    2019/6/22 更新了剩余接口

--- src/main/java/com/ynu/finalwork/controller/TeachController.java
+++ src/main/java/com/ynu/finalwork/controller/TeachController.java
@@ -2,32 +2,35 @@ package com.ynu.finalwork.controller;
 
 
 import com.alibaba.fastjson.JSONObject;
+import com.ynu.finalwork.entity.Student;
 import com.ynu.finalwork.entity.Teach;
 import com.ynu.finalwork.repository.TeachRepository;
+import com.ynu.finalwork.service.CourseService;
+import com.ynu.finalwork.service.ElectCourseService;
 import com.ynu.finalwork.service.TeachService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/teach")
 public class TeachController {
     @Autowired
-    private TeachService teachService;
+    TeachService teachService;
+    @Autowired
+    ElectCourseService electCourseService;
 
     JSONObject jsonObject;
     //新增和更新同理
     @PostMapping("/add")
-    public Object teachAdd(@RequestParam("cid")Integer cid,
-                           @RequestParam("tid")Integer tid){
+    public Object teachAdd(Teach teach){
         jsonObject = new JSONObject();
 //        若通过cid找到的教师存在，则不能加入
-        if(teachService.findTeacherByCid(cid)!=null||teachService.findCourseByTid(tid)!= null){
+        if(teachService.findTeacherByCid(teach.getCid())!=null||teachService.findCourseByTid(teach.getTid())!= null){
             jsonObject.put("message","安排失败,已存在课程!");
         }
-        Teach teach = new Teach();
-        teach.setTid(tid);
-        teach.setCid(cid);
         jsonObject.put("record",teach);
         teachService.addTeach(teach);
         return jsonObject;
